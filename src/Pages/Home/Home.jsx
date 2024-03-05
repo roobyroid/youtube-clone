3133e0146807b7aa0446f0e9a1a89ab5b1133c3d
Oleg <oopolunin@gmail.com>
2024-03-05 16:19:19 +0300

    create feed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Feed from '../../Components/Feed/Feed';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './Home.css';
 
 const Home = ({ sidebar }) => {
-	return <Sidebar sidebar={sidebar} />;
+	return (
+		<>
+			<Sidebar sidebar={sidebar} />
+			<div className={sidebar ? 'container' : 'container container--large'}>
+				<Feed />
+			</div>
+		</>
+	);
 };
 
 export default Home;
